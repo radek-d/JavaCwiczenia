6bd8cdd2b9f91d591f6f63e08427d13f5c984f6a
radek-d <revrd10@gmail.com>
2025-09-23 09:26:17 +0200

    zad1

--- zadania1/src/Main.java
+++ zadania1/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //zad1
+        System.out.println("Zadanie 1");
         double stopnieC, stopnieF;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj stopnie Celsjusza: ");
@@ -10,6 +11,40 @@ public class Main {
 
         stopnieF= 1.8 * stopnieC + 32;
         System.out.println("Stopnie Fahrenheita " + stopnieF);
+        //zad2
+        System.out.println("Zadanie 2");
+        double a, b, c;
+        Scanner scannerliczb = new Scanner(System.in);
 
+        System.out.println("Podaj A: ");
+        a = scannerliczb.nextDouble();
+
+        System.out.println("Podaj B: ");
+        b = scannerliczb.nextDouble();
+
+        System.out.println("Podaj C: ");
+        c = scannerliczb.nextDouble();
+
+        if (a >= b && a >= c) {
+            if (b <= c) {
+                System.out.println("Największa liczba to " + a + ", a najmniejsza to " + b);
+            } else {
+                System.out.println("Największa liczba to " + a + ", a najmniejsza to " + c);
+            }
+        } else if (b >= a && b >= c) {
+            if (a <= c) {
+                System.out.println("Największa liczba to " + b + ", a najmniejsza to " + a);
+            } else {
+                System.out.println("Największa liczba to " + b + ", a najmniejsza to " + c);
+            }
+        } else {
+            if (a <= b) {
+                System.out.println("Największa liczba to " + c + ", a najmniejsza to " + a);
+            } else {
+                System.out.println("Największa liczba to " + c + ", a najmniejsza to " + b);
+            }
+        }
+
+        scanner.close();
     }
 }
